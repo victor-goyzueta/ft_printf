2b37348f13d7aee291c02d1fb3a505c25c9dea87
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2024-10-30 10:48:40 +0100

    init ft_printf

---
+++ ft_printf.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 17:42:47 by vgoyzuet          #+#    #+#             */
+/*   Updated: 2024/10/29 18:50:45 by vgoyzuet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+static int  *format()
+{
+    
+}
+
+int ft_printf(char const *s, ...)
+{
+    va_list args;
+
+    va_start(args, format);
+}
