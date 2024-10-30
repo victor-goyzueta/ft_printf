89ed1a31b286c4cce9b6a7a04c7b3def44b24f9d
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2024-10-30 18:03:55 +0100

    update ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,20 +6,38 @@
 /*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:42:47 by vgoyzuet          #+#    #+#             */
-/*   Updated: 2024/10/29 18:50:45 by vgoyzuet         ###   ########.fr       */
+/*   Updated: 2024/10/30 16:00:40 by vgoyzuet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-static int  *format()
+static int	conver_type(va_list args, char const *format, int *counts)
 {
-    
+
 }
 
-int ft_printf(char const *s, ...)
+int ft_printf(char const *format, ...)
 {
-    va_list args;
+	va_list args;
+	int	counts;
 
-    va_start(args, format);
+	counts = 0;
+	va_start(args, format);
+	while (*format)
+	{
+        if (*format == '%')
+		{
+            format++;
+            if (conver_type(args, *format, &counts) == -1)
+                return -1;  // Error en el especificador
+        }
+        else
+		{
+            counts += write(1, format, 1);
+        }
+        format++;
+    }
+	va_end(args);
+	return(counts);
 }
