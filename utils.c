7b2e132a031d24c9d46cdb7509c005531579d0b4
victor-goyzueta <vgoyzuet@c1r13s5.42madrid.com>
2024-11-05 21:48:43 +0100

    update

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 16:37:27 by vgoyzuet          #+#    #+#             */
-/*   Updated: 2024/11/05 21:43:11 by vgoyzuet         ###   ########.fr       */
+/*   Updated: 2024/11/05 21:48:17 by vgoyzuet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ int	ft_puthex(unsigned long num, int *counts, char format)
 	else
 		base = "0123456789ABCDEF";
 	if (num >= 16)
-		ft_puthex(num / 16, counts);
+		ft_puthex(num / 16, counts, format);
 	else
 		counts += ft_putchar(base[num % 16], counts);
 	return (*counts);
